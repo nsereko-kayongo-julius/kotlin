20601c20ad4712dcb23720620bf93112ef46da3b
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-05-24 08:33:20 +0300

    variables and string literals in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,33 @@
-fun main(args: Array<String>) {
-   val sum = add(2,2)
-    println(sum)
+fun main() {
+
+    val a: Int
+        a = 5
+    val b = 6
+
+    val max: Int
+    if (a >= b){
+        max = a
+    }else {
+        max = b
+    }
+
+    println("max = $max")
+
+    val day =  0
+    when (day){
+        1 -> print("$day = Monday")
+        2 -> print("$day = tuesday")
+        3 -> print("$day = wednesday")
+        4 -> print("$day =thursday")
+        5 -> print("$day = friday")
+        6 -> print("$day = saturday")
+        7 -> print("$day = sunday")
+        else ->{
+            print("fuck you only numbers")
+        }
+
+    }
+
+
+
 }
-// noral functions
-fun add ( a: Int, b: Int): Int {
-    return a+b
-};
