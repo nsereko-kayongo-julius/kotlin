abe85fac2bcb65282c78b6789ca4a0ba1a5ce7a6
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-05-24 08:30:53 +0300

    inheritance in kotlin and visibility modifiers

---
+++ src/main/kotlin/session5.kt
@@ -0,0 +1,19 @@
+open class Shape{
+    private var name:String = ""
+    var number: Int = 0
+    open protected var type: String = ""
+
+}
+
+open class Circle(x: String) : Shape(){
+override var type: String = x
+
+}
+
+
+
+object DatabaseManager {
+
+
+}
+
